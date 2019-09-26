b3d6454ad9a0220877ce66b9aa4928a9ae254740
Arthur Vernet <assa@tsf-436-wpa-6-197.epfl.ch>
2019-09-26 14:45:11 +0200

    space function

--- in/rabbits/vernet-wust-in/src/RabbitsGrassSimulationSpace.java
+++ in/rabbits/vernet-wust-in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,20 @@
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
 
 public class RabbitsGrassSimulationSpace {
+    private Object2DGrid rabbitsGrassSpace;
+
+    public RabbitsGrassSimulationSpace(int xSize, int ySize){
+        rabbitsGrassSpace = new Object2DGrid(xSize, ySize);
+        for(int i = 0; i < xSize; i++){
+            for(int j = 0; j < ySize; j++){
+                rabbitsGrassSpace.putObjectAt(i, j, new Integer(0));
+            }
+        }
+    }
 
 }
