0b17f6460ea7b0083f2b2b42b828a2bf037a1eaa
MattW0 <matthias.wust@epfl.ch>
2019-10-17 15:09:14 +0200

    graph and bfs setup

--- ex3/deliberative/vernet-wust-ex3/src/template/DeliberativeTemplate.java
+++ ex3/deliberative/vernet-wust-ex3/src/template/DeliberativeTemplate.java
@@ -11,21 +11,26 @@ import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
+import java.security.Key;
+import java.util.*;
+
 /**
  * An optimal planner for one vehicle.
  */
 @SuppressWarnings("unused")
 public class DeliberativeTemplate implements DeliberativeBehavior {
 
-	enum Algorithm { BFS, ASTAR }
+	enum Algorithm { NAIVE, BFS, ASTAR }
 	
 	/* Environment */
 	Topology topology;
 	TaskDistribution td;
+	int numCities;
 	
 	/* the properties of the agent */
 	Agent agent;
 	int capacity;
+	int costPerKm;
 
 	/* the planning class */
 	Algorithm algorithm;
@@ -38,6 +43,10 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		
 		// initialize the planner
 		int capacity = agent.vehicles().get(0).capacity();
+		int costPerKm = agent.vehicles().get(0).costPerKm();
+		int numCities = topology.cities().size();
+		System.out.println("Number of cities: " + numCities);
+
 		String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
 		
 		// Throws IllegalArgumentException if algorithm is unknown
@@ -52,13 +61,17 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 
 		// Compute the plan with the selected algorithm.
 		switch (algorithm) {
-		case ASTAR:
+		case NAIVE:
 			// ...
 			plan = naivePlan(vehicle, tasks);
 			break;
+		case ASTAR:
+			// ...
+			plan = astarPlan(vehicle, tasks);
+			break;
 		case BFS:
 			// ...
-			plan = naivePlan(vehicle, tasks);
+			plan = bfsPlan(vehicle, tasks);
 			break;
 		default:
 			throw new AssertionError("Should not happen.");
@@ -89,6 +102,107 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 		return plan;
 	}
 
+	private Plan astarPlan(Vehicle vehicle, TaskSet tasks) {
+        Plan plan = new Plan(vehicle.getCurrentCity());
+		return plan;
+	}
+
+	private Plan bfsPlan(Vehicle vehicle, TaskSet tasks) {
+
+	    //define initial state & all possible goal states (city = delivery/pickup city, currentTasks = null, tasksLeft = 0)
+        final List<Task> initialTasks = getInitialTasks(tasks);
+        final List<State> goalStates = evaluateGoalStates(tasks);
+        int numberOfGoalStates = goalStates.size();
+
+		State initialState = new State(vehicle.getCurrentCity(), null, initialTasks);
+
+        //BFS Algorithm
+        List<State> actions = bfsAlgorithm(initialState, initialTasks, goalStates);
+
+		Plan plan = new Plan(initialState.city);
+
+		return plan;
+	}
+
+	public List<DeliberativeTemplate.State> bfsAlgorithm(DeliberativeTemplate.State currentState, List<Task> remainingTasks, List<DeliberativeTemplate.State> goalStates){
+
+		//Final list with all state transitions (= plan)
+		Map<Integer, DeliberativeTemplate.State> nodes = new HashMap<>();
+		List<DeliberativeTemplate.State> actions = new LinkedList<>();
+
+		int depthLvl = 0;
+		int nodeIndex = 0;
+		int totalWeight = 0;
+
+		List<Topology.City> neighbors;
+
+		//Lists with all pickup and delivery destinations
+		List<Topology.City> deliveries = new ArrayList<>(remainingTasks.size());
+		List<Topology.City> pickups = new ArrayList<>(remainingTasks.size());
+
+		System.out.println("TaskSet:");
+		System.out.println(remainingTasks.toString());
+
+		for(int index = 0; index < remainingTasks.size(); index++){
+			Task task = remainingTasks.get(index);
+			deliveries.add(task.deliveryCity);
+			pickups.add(task.pickupCity);
+		}
+
+		DeliberativeTemplate.State nextState = currentState;
+
+		do {
+
+			//is delivery possible?
+			if(deliveries.contains(currentState.city)){
+				for(Task task : currentState.currentTasks){
+					//always make a delivery
+					if(task.deliveryCity.equals(currentState.city)) {
+						nextState.currentTasks.remove(task);
+						nodes.put(nodeIndex, nextState);
+						nodeIndex++;
+					}
+				}
+			}
+
+			//is pickup possible?
+			if(pickups.contains(currentState.city)){
+				for(Task task: currentState.currentTasks){
+					totalWeight += task.weight;
+				}
+				for(Task task : currentState.remainingTasks) {
+					//add all possible pickups to nodes
+					if (task.pickupCity.equals(currentState.city) && (totalWeight + task.weight) <= capacity) {
+						nextState.remainingTasks.remove(task);
+						nextState.currentTasks.add(task);
+						//already visited?
+						if(!nodes.containsValue(nextState)){
+							nodes.put(nodeIndex, nextState);
+							nodeIndex++;
+						}
+						else{
+
+						}
+					}
+				}
+			}
+
+			neighbors = currentState.city.neighbors();
+			while(neighbors.iterator().hasNext()){
+				nextState.city = neighbors.iterator().next();
+				if(!nodes.containsValue(nextState)){
+					nodes.put(nodeIndex, nextState);
+					nodeIndex++;
+				}
+			}
+
+			depthLvl++;
+
+		} while(!goalStates.contains(nextState));
+
+		return actions;
+	}
+
 	@Override
 	public void planCancelled(TaskSet carriedTasks) {
 		
@@ -98,4 +212,67 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
 			// plan is computed.
 		}
 	}
+
+	private List<State> evaluateGoalStates(TaskSet tasks){
+        List<State> goalStates = new ArrayList<>();
+
+        for (Task task : tasks) {
+			State goalStateDel = new State(task.deliveryCity, null, null);
+			State goalStatePU = new State(task.pickupCity, null, null);
+            if(!goalStates.contains(goalStatePU))
+                goalStates.add(goalStatePU);
+			if(!goalStates.contains(goalStateDel))
+				goalStates.add(goalStateDel);
+        }
+        return goalStates;
+    }
+
+    private List<Task> getInitialTasks(TaskSet tasks){
+        List<Task> initialTasks = new ArrayList<>();
+
+        for (Task task : tasks)
+            initialTasks.add(task);
+
+        return initialTasks;
+    }
+
+	private State pickup(State currentState, List<Task> remainingTasks, int totalWeight){
+
+		State state = currentState;
+		//check if multiple tasks can be picked up and if the capacity would not be exceeded
+		for(Task task : currentState.remainingTasks){
+			if(task.pickupCity.equals(currentState.city) && (totalWeight + task.weight) <= capacity){
+				currentState.currentTasks.add(task);
+				return state;
+			}
+		}
+		return state;
+	}
+
+    private class State {
+        private City city;
+        private List<Task> currentTasks;
+        private List<Task> remainingTasks;
+
+        public State(City city, List<Task> currentTasks, List<Task> remainingTasks) {
+            this.city = city;
+            this.currentTasks = currentTasks;
+            this.remainingTasks = remainingTasks;
+        }
+
+		//what if conditions must be met?
+        @Override
+        public boolean equals(Object obj) {
+            if(obj != null && obj instanceof State) {
+                State s = (State)obj;
+                if(currentTasks != null && s.currentTasks != null) {
+                    return city.equals(s.city) && currentTasks.equals(s.currentTasks);
+                } else if (currentTasks == null && s.currentTasks == null){
+                    return city.equals(s.city);
+                }
+            }
+            return false;
+        }
+    }
+
 }
