e859198e01d3514a8fff6e95573b7e3aebc657a8
Arthur Vernet <arthurvernet@gmail.com>
2019-11-02 16:41:57 +0100

    cleaned code and made more efficient classes

--- ex4/centralized/vernet-wust-ex4/src/template/CentralizedTemplate.java
+++ ex4/centralized/vernet-wust-ex4/src/template/CentralizedTemplate.java
@@ -3,7 +3,10 @@ package template;
 //the list of imports
 import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import com.sun.source.doctree.ValueTree;
 import logist.LogistSettings;
 
 import logist.Measures;
@@ -19,12 +22,7 @@ import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
-/**
- * A very simple auction agent that assigns all tasks to its first vehicle and
- * handles them sequentially.
- *
- */
-@SuppressWarnings("unused")
+
 public class CentralizedTemplate implements CentralizedBehavior {
 
     private Topology topology;
@@ -32,25 +30,24 @@ public class CentralizedTemplate implements CentralizedBehavior {
     private Agent agent;
     private long timeout_setup;
     private long timeout_plan;
-    
+
     @Override
     public void setup(Topology topology, TaskDistribution distribution,
-            Agent agent) {
-        
+                      Agent agent) {
+
         // this code is used to get the timeouts
         LogistSettings ls = null;
         try {
             ls = Parsers.parseSettings("config" + File.separator + "settings_default.xml");
-        }
-        catch (Exception exc) {
+        } catch (Exception exc) {
             System.out.println("There was a problem loading the configuration file.");
         }
-        
+
         // the setup method cannot last more than timeout_setup milliseconds
         timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
         // the plan method cannot execute more than timeout_plan milliseconds
         timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
-        
+
         this.topology = topology;
         this.distribution = distribution;
         this.agent = agent;
@@ -63,11 +60,13 @@ public class CentralizedTemplate implements CentralizedBehavior {
         int nb_vehicles = vehicles.size();
         int nb_tasks = tasks.size();
 
-        //List<Object> X = createVariables(vehicles, tasks);
-        List<List<Object>> D = createDomains(vehicles, tasks);
+        Variable X = new Variable(vehicles, tasks);
+        Domain D = new Domain(vehicles, tasks);
+        Constraint C = new Constraint();
 
         System.out.println("Agent " + agent.id() + " has tasks " + tasks);
 
+        /*
         Plan planVehicle1 = naivePlan(vehicles.get(0), tasks);
 
         List<Plan> plans = new ArrayList<Plan>();
@@ -75,13 +74,47 @@ public class CentralizedTemplate implements CentralizedBehavior {
         while (plans.size() < vehicles.size()) {
             plans.add(Plan.EMPTY);
         }
+        */
 
+        List<Plan> plans = optimalPlans(X, D, C);
 
         long time_end = System.currentTimeMillis();
         long duration = time_end - time_start;
         System.out.println("The plan was generated in " + duration + " milliseconds.");
-        
+
         return plans;
+    }
+
+    private List<Plan> optimalPlans(Variable X, Domain D, Constraint C){
+        Assignment solution = stochasticLocalSearch(X, D, C);
+        List<Plan> plans = plansFromVariableAssignment(solution);
+
+        return plans;
+    }
+
+    private Assignment stochasticLocalSearch(Variable X, Domain D, Constraint C){
+        Assignment A = selectInitialSolution(X, D, C);
+        return A;
+    }
+
+    private Assignment selectInitialSolution(Variable X, Domain D, Constraint C) {
+        List<Task> tasks = D.nextTask;
+        for(int i = 0; i < tasks.size() - 1; i++){
+            X.nextTask_t.put(tasks.get(i), tasks.get(i+1));
+            X.time.put(tasks.get(i), i + 1);
+            X.vehicle.put(tasks.get(i), D.vehicle.get(0));
+        }
+        X.nextTask_t.put(tasks.get(tasks.size() - 1), null);
+        X.time.put(tasks.get(tasks.size() - 1), tasks.size());
+
+        X.nextTask_v.put(D.vehicle.get(0), tasks.get(0));
+
+        return new Assignment(X, D, C);
+    }
+
+    //TODO
+    private List<Plan> plansFromVariableAssignment(Assignment A){
+        return null;
     }
 
     private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
@@ -109,205 +142,200 @@ public class CentralizedTemplate implements CentralizedBehavior {
         return plan;
     }
 
-    //private Plan SLS(variables, domains, constraints, costFunction)
+    private class Assignment{
 
-    //domains: defined once and doesn't change
-    private final List<List<Object>> createDomains(List<Vehicle> vehicles, TaskSet tasks){
-        int nb_tasks = tasks.size();
-        int nb_vehicles = vehicles.size();
+        private Variable X;
+        private Domain D;
+        private Constraint C;
+        private double cost;
 
-        List<List<Object>> domains = new Vector<>(3);
-        List<Object> nextTasks = new Vector<>(nb_tasks+1); //+1: NULL element
-        List<Object> time = new Vector<>(nb_tasks);
-        List<Object> vehicle = new Vector<>(nb_tasks);
-
-        int i = 1;
-        for(Task task : tasks) {
-            nextTasks.add(task);
-            time.add(i);
-            if(i<=nb_vehicles)
-                vehicle.add(i);
-            i++;
+        public Assignment(Variable X, Domain D, Constraint C){
+            assert(isValid());
+            this.X = X;
+            this.D = D;
+            this.C = C;
+            this.cost = cost();
         }
-        nextTasks.add(null);
 
-        domains.add(nextTasks);
-        domains.add(time);
-        domains.add(vehicle);
+        private boolean isValid(){
 
-        return domains;
-    }
+            boolean b1 = C.c1(X.nextTask_t);
+            boolean b2 = C.c2(X.nextTask_v, X.time);
+            boolean b3 = C.c3(X.nextTask_t, X.time);
+            boolean b4 = C.c4(X.nextTask_v, X.vehicle);
+            boolean b5 = C.c5(X.nextTask_t, X.vehicle);
+            boolean b6 = C.c6(X.nextTask_t, X.nextTask_v, D.nextTask);
+            boolean b7 = C.c7(X.vehicle);
 
-    //constraints: 7 types (3.1)
-    private final List<Object> createConstraints(List<Object> X){
-        List<Object> C = new Vector<>(7);
-
-        return C;
-    }
-
-    //arguments: all tasks & X.nextTask_t
-    //TRUE: nextTask(task) != task
-    public boolean c1(TaskSet tasks, Map<Task, Task> nextTask_t){
-
-        for (Task task : tasks){
-            if(!nextTask_t.get(task).equals(null) && nextTask_t.get(task).equals(task))
-                return false;
+            return b1 && b2 && b3 && b4 && b5 && b6 && b7;
         }
-        return true;
-    }
 
-    //argument: X.nextTask_v
-    //TRUE: first task of every vehicle is at time 1;
-    public boolean c2(Map<Vehicle, Task> nextTask_v){
+        //TODO: the cost function is most likely not adapted for vehicles carrying several tasks
+        private double cost(){
+            double cost = 0;
 
-        for(Vehicle v: nextTask_v.keySet()){
-            if(!nextTask_v.get(v).equals(null) && !nextTask_v.get(v).equals(1))
-                return false;
+            for(Task task: D.nextTask){
+                Task nextTask = X.nextTask_t.get(task);
+                if(nextTask != null) {
+                    double distance = task.deliveryCity.distanceTo(nextTask.pickupCity);
+                    double length = nextTask.pickupCity.distanceTo(nextTask.deliveryCity);
+                    cost += (distance + length) * X.vehicle.get(task).costPerKm();
+                }
+            }
+
+            for(Vehicle vehicle: D.vehicle){
+                Task nextTask = X.nextTask_v.get(vehicle);
+                if(nextTask != null) {
+                    double distance = vehicle.homeCity().distanceTo(nextTask.pickupCity);
+                    double length = nextTask.pickupCity.distanceTo(nextTask.deliveryCity);
+                    cost += (distance + length) * vehicle.costPerKm();
+                }
+            }
+
+            return cost;
         }
-        return true;
+
     }
 
-    //arguments: X.nextTask_t & X.time
-    //TRUE: time of nextTask = time of currentTask + 1
-    public boolean c3(Map<Task, Task> nextTask_t, Map<Task, Integer> time){
+    private class Variable {
 
-        for(Task t : nextTask_t.keySet()){
-            if(!nextTask_t.get(t).equals(null)){
-                if (time.get(nextTask_t.get(t)) != time.get(t) + 1)
-                    return false;
+        private Map<Task, Task> nextTask_t;
+        private Map<Vehicle, Task> nextTask_v;
+        private Map<Task, Integer> time;
+        private Map<Task, Vehicle> vehicle;
+
+        public Variable(List<Vehicle> vehicles, TaskSet tasks) {
+
+            this.nextTask_t = new HashMap<>(tasks.size());
+            this.nextTask_v = new HashMap<>(vehicles.size());
+            this.time = new HashMap<>(tasks.size());
+            this.vehicle = new HashMap<>(tasks.size());
+
+            for(Task task: tasks){
+                nextTask_t.put(task, null);
+                time.put(task, null);
+                vehicle.put(task, null);
+            }
+
+            for(Vehicle vehicle: vehicles){
+                nextTask_v.put(vehicle, null);
             }
         }
-        return true;
     }
 
-    //arguments: X.nextTask_v & X.vehicles
-    //TRUE: redundant declaration of vehicle(task) = task(vehicle)
-    public boolean c4(Map<Vehicle, Task> nextTask_v, Map<Task, Vehicle> vehicles){
+    private class Domain {
 
-        for(Vehicle v : nextTask_v.keySet()){
-            if(!nextTask_v.get(v).equals(null)){
-                if(vehicles.get(nextTask_v.get(v)) != v)
+        private List<Task> nextTask;
+        private List<Integer> time;
+        private List<Vehicle> vehicle;
+
+        public Domain(List<Vehicle> vehicles, TaskSet tasks){
+            this.nextTask = new ArrayList<>(tasks);
+            this.time = IntStream.rangeClosed(1, tasks.size()).boxed().collect(Collectors.toList());
+            this.vehicle = new ArrayList<>(vehicles);
+        }
+
+    }
+
+    private class Constraint {
+
+        //nextTask(task) != task
+        private boolean c1(Map<Task, Task> nextTask_t) {
+
+            for (Map.Entry<Task, Task> entry : nextTask_t.entrySet()) {
+                if(entry.getValue().equals(entry.getKey())){
                     return false;
+                }
             }
-        }
-        return true;
-    }
 
-    //arguments: X.nextTask_t & X.vehicles
-    //TRUE: nextTask(t1) = t2 -> vehicle(t1) = vehicle(t2)
-    public boolean c5(Map<Task, Task> nextTask_t, Map<Task, Vehicle> vehicles){
-
-        for(Task t : nextTask_t.keySet()){
-            if(!nextTask_t.get(t).equals(null)){
-                if(vehicles.get(nextTask_t.get(t)) != vehicles.get(t))
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    //arguments: X.nextTask_t & X.nextTask_v & all tasks
-    //TRUE: nextTask(t&v) contain all tasks + nbVehicles times a null argument
-    public boolean c6(Map<Task, Task> nextTask_t,  Map<Vehicle, Task> nextTask_v, TaskSet tasks){
-
-        Set<Task> tasksToCheck = new HashSet<>();
-        tasksToCheck.addAll(tasks);
-
-        int nullCount = 0;
-
-        for(Vehicle v : nextTask_v.keySet()){
-            if(nextTask_v.get(v).equals(null))
-                nullCount++;
-            else if(tasks.contains(nextTask_v.get(v)))
-                tasksToCheck.remove(nextTask_v.get(v));
-        }
-
-        for(Task t : nextTask_t.keySet()){
-            if(nextTask_t.get(t).equals(null))
-                nullCount++;
-            else if(tasks.contains(nextTask_t.get(t)))
-                tasksToCheck.remove(nextTask_t.get(t));
-        }
-
-        if(nullCount==nextTask_v.size() && tasksToCheck.size()==0)
             return true;
-
-        return false;
-    }
-
-    //argument: X.vehicles
-    //TRUE: No task will exceed the capacity of a vehicle
-    //TODO: currently implemented for only one task
-    public boolean c7(Map<Task, Vehicle> vehicles){
-
-        for(Task t : vehicles.keySet()){
-            if(t.weight > vehicles.get(t).capacity())
-                return false;
         }
 
-        return true;
+        //first task of every vehicle is at time 1
+        private boolean c2(Map<Vehicle, Task> nextTask_v, Map<Task, Integer> time) {
+
+            for (Task task : nextTask_v.values()) {
+                if (time.get(task) != 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //time of nextTask = time of currentTask + 1
+        private boolean c3(Map<Task, Task> nextTask_t, Map<Task, Integer> time) {
+
+            for (Map.Entry<Task, Task> entry : nextTask_t.entrySet()) {
+                    if(time.get(entry.getValue()) != time.get(entry.getKey()) + 1)
+                        return false;
+            }
+            return true;
+        }
+
+        //redundant declaration of vehicle(task) = task(vehicle)
+        private boolean c4(Map<Vehicle, Task> nextTask_v, Map<Task, Vehicle> vehicle) {
+
+            for (Map.Entry<Vehicle, Task> entry : nextTask_v.entrySet()) {
+                if(!vehicle.get(entry.getValue()).equals(entry.getKey())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //nextTask(t1) = t2 -> vehicle(t1) = vehicle(t2)
+        private boolean c5(Map<Task, Task> nextTask_t, Map<Task, Vehicle> vehicle) {
+
+            for (Map.Entry<Task, Task> entry : nextTask_t.entrySet()) {
+                if(!vehicle.get(entry.getValue()).equals(vehicle.get(entry.getKey()))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //nextTask(t&v) contain all tasks + nbVehicles times a null argument
+        private boolean c6(Map<Task, Task> nextTask_t, Map<Vehicle, Task> nextTask_v, List<Task> tasks) {
+
+            Set<Task> tasksToCheck = new HashSet<>();
+            tasksToCheck.addAll(tasks);
+
+            int nullCount = 0;
+
+            for (Vehicle v : nextTask_v.keySet()) {
+                if (nextTask_v.get(v) == null) {
+                    nullCount++;
+                } else if (tasks.contains(nextTask_v.get(v))) {
+                    tasksToCheck.remove(nextTask_v.get(v));
+                }
+            }
+
+            for (Task t : nextTask_t.keySet()) {
+                if (nextTask_t.get(t) == null) {
+                    nullCount++;
+                } else if (tasks.contains(nextTask_t.get(t))) {
+                    tasksToCheck.remove(nextTask_t.get(t));
+                }
+            }
+
+            return nullCount == nextTask_v.size() && tasksToCheck.size() == 0;
+        }
+
+        //No task will exceed the capacity of a vehicle
+        private boolean c7(Map<Task, Vehicle> vehicle) {
+
+            Map<Vehicle, Integer> load = new HashMap<>();
+
+            for (Map.Entry<Task, Vehicle> entry : vehicle.entrySet()) {
+                Task t = entry.getKey();
+                Vehicle v = entry.getValue();
+                load.put(v, load.get(v) + t.weight);
+                if (load.get(v) > v.capacity()) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
-}
-
-//Variables: class of 3*nb_tasks + nb_vehicles elements
-class X {
-    public Map<Task, Task> nextTask_t;
-    public Map<Vehicle, Task> nextTask_v;
-    public Map<Task, Integer> time;
-    public Map<Task, Vehicle> vehicles;
-
-    public X(List<Vehicle> v, TaskSet tasks){
-        this.nextTask_t = createNextTask_tMap(tasks);
-        this.nextTask_v = createNextTask_vMap(v);
-        this.time = createTimeMap(tasks);
-        this.vehicles = createVehicleMap(v, tasks);
-    }
-
-    //TODO Values are currently all null, only basic structure for mapping of varables
-    private Map<Task, Task> createNextTask_tMap(TaskSet tasks){
-
-        Map<Task, Task> nextTask = new HashMap<>(tasks.size());
-
-        //next task after current task
-        for(Task task : tasks)
-            nextTask.put(task, null);
-
-        return nextTask;
-    }
-
-    private Map<Vehicle, Task> createNextTask_vMap(List<Vehicle> v){
-
-        Map<Vehicle, Task> nextTask_v = new HashMap<>(v.size());
-
-        //first task for vehicle
-        for(Vehicle vehicle : v)
-            nextTask_v.put(vehicle, null);
-
-        return nextTask_v;
-    }
-
-    private Map<Task, Integer> createTimeMap(TaskSet tasks){
-
-        Map<Task, Integer> time = new HashMap<>(tasks.size());
-
-        for(Task task : tasks)
-            time.put(task, 0);
-
-        return time;
-    }
-
-    private Map<Task, Vehicle> createVehicleMap(List<Vehicle> v, TaskSet tasks){
-
-        Map<Task, Vehicle> vehicles = new HashMap<>(v.size());
-
-        for (Task task : tasks)
-            vehicles.put(task, null);
-
-        return vehicles;
-    }
-
-}
-
-class assignment {
-
 }
